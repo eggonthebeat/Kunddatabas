107c393b12b149e9b651933409449e240f39af18
eggonthebeat <calcium2k@gmail.com>
2022-11-18 09:38:04 +0100

    Add project files.

---
+++ Kunddatabas/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿
+namespace Kunddatabas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.bert = new System.Windows.Forms.TableLayoutPanel();
+            this.Add = new System.Windows.Forms.Button();
+            this.postadress = new System.Windows.Forms.TextBox();
+            this.distributionstyp = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.gatuadress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.namn = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tabort = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(366, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Återställ Databas";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 171);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sök på namn, adress, distributiontyp eller epost";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(366, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 21);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Sök";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Namn",
+            "Gatuadress",
+            "Postadress",
+            "Distributionstyp"});
+            this.comboBox1.Location = new System.Drawing.Point(239, 171);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Text = "Namn";
+            // 
+            // bert
+            // 
+            this.bert.BackColor = System.Drawing.Color.White;
+            this.bert.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.bert.ColumnCount = 4;
+            this.bert.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.bert.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.bert.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.bert.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.bert.Location = new System.Drawing.Point(32, 198);
+            this.bert.Name = "bert";
+            this.bert.RowCount = 2;
+            this.bert.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.bert.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.bert.Size = new System.Drawing.Size(447, 240);
+            this.bert.TabIndex = 8;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(32, 103);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(201, 23);
+            this.Add.TabIndex = 9;
+            this.Add.Text = "Lägg till kund";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // postadress
+            // 
+            this.postadress.Location = new System.Drawing.Point(239, 71);
+            this.postadress.Name = "postadress";
+            this.postadress.Size = new System.Drawing.Size(74, 20);
+            this.postadress.TabIndex = 1;
+            this.postadress.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // distributionstyp
+            // 
+            this.distributionstyp.FormattingEnabled = true;
+            this.distributionstyp.Items.AddRange(new object[] {
+            "Kivra",
+            "Email",
+            "Post",
+            "Efaktura"});
+            this.distributionstyp.Location = new System.Drawing.Point(239, 22);
+            this.distributionstyp.Name = "distributionstyp";
+            this.distributionstyp.Size = new System.Drawing.Size(74, 21);
+            this.distributionstyp.TabIndex = 10;
+            this.distributionstyp.Text = "Efaktura";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(235, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Distributionstyp";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(236, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Postadress";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // gatuadress
+            // 
+            this.gatuadress.Location = new System.Drawing.Point(32, 71);
+            this.gatuadress.Name = "gatuadress";
+            this.gatuadress.Size = new System.Drawing.Size(192, 20);
+            this.gatuadress.TabIndex = 1;
+            this.gatuadress.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Gatuadress";
+            // 
+            // namn
+            // 
+            this.namn.Location = new System.Drawing.Point(32, 23);
+            this.namn.Name = "namn";
+            this.namn.Size = new System.Drawing.Size(192, 20);
+            this.namn.TabIndex = 1;
+            this.namn.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 6);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Namn";
+            // 
+            // tabort
+            // 
+            this.tabort.Location = new System.Drawing.Point(414, 71);
+            this.tabort.Name = "tabort";
+            this.tabort.Size = new System.Drawing.Size(84, 20);
+            this.tabort.TabIndex = 0;
+            this.tabort.Text = "Ta bort kund";
+            this.tabort.UseVisualStyleBackColor = true;
+            this.tabort.Click += new System.EventHandler(this.tabort_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(366, 71);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(42, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(363, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "ID";
+            this.label6.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.distributionstyp);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.bert);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.namn);
+            this.Controls.Add(this.gatuadress);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.postadress);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tabort);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TableLayoutPanel bert;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.TextBox postadress;
+        private System.Windows.Forms.ComboBox distributionstyp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox gatuadress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox namn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button tabort;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Kunddatabas/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using System.Data.SQLite;
+using System.Data.SQLite.EF6;
+using System.Data.SQLite.Generic;
+using System.Data.SQLite.Linq;
+
+namespace Kunddatabas
+{
+    public partial class Form1 : Form
+    {
+
+        string stm = "SELECT SQLITE_VERSION()";
+
+        string cs = @"URI=file:C:\Users\calci\OneDrive\Dokument\Database\kundupgifter.db"; // path hosted locally
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var con = new SQLiteConnection(cs);
+            con.Open();
+
+            var cmd = new SQLiteCommand(stm, con);
+            string version = cmd.ExecuteScalar().ToString();
+
+            Console.WriteLine($"SQLite version: {version}");
+            Console.WriteLine("Updating database");
+
+
+            string[] tables = { "t_person", "t_adress", "t_distribution" };
+
+            foreach(var table in tables)
+            {
+                command("DROP TABLE IF EXISTS "+table, cmd);
+                
+            }
+            command("CREATE TABLE t_person(id INTEGER PRIMARY KEY, name TEXT)", cmd);
+            command("CREATE TABLE t_adress(id INTEGER PRIMARY KEY, gatuadress TEXT, postadress INT)", cmd);
+            command("CREATE TABLE t_distribution(id INTEGER PRIMARY KEY, distributionstyp TEXT)", cmd);
+            //delete
+
+
+            //create
+
+            // Post, Email, Kivra, Efaktura
+
+            //fill
+            command("INSERT INTO t_person(name) VALUES('Johan Nilsson')", cmd);
+                command("INSERT INTO t_adress(gatuadress, postadress) VALUES('1019 Oak Lane', '65330')", cmd);
+                command("INSERT INTO t_distribution(distributionstyp) VALUES('Kivra')", cmd);
+
+            command("INSERT INTO t_person(name) VALUES('Georg Gunnar')", cmd);
+                command("INSERT INTO t_adress(gatuadress, postadress) VALUES('4665 Marshall Street', '20707')", cmd);
+                command("INSERT INTO t_distribution(distributionstyp) VALUES('Email')", cmd);
+
+            command("INSERT INTO t_person(name) VALUES('Buse Burre')", cmd);
+                command("INSERT INTO t_adress(gatuadress, postadress) VALUES('2071 Franklin Avenue', '78380')", cmd);
+                command("INSERT INTO t_distribution(distributionstyp) VALUES('Efaktura')", cmd);
+
+            command("INSERT INTO t_person(name) VALUES('Vilson Kycklin')", cmd);
+                command("INSERT INTO t_adress(gatuadress, postadress) VALUES('98 Whaley Lane', '78380')", cmd);
+                command("INSERT INTO t_distribution(distributionstyp) VALUES('Post')", cmd);
+
+            command("INSERT INTO t_person(name) VALUES('Koppe Ragnarok')", cmd);
+                command("INSERT INTO t_adress(gatuadress, postadress) VALUES('921 Gandy Street', '13202')", cmd);
+                command("INSERT INTO t_distribution(distributionstyp) VALUES('Kivra')", cmd);
+            
+
+            Console.WriteLine("Johan och Gunnar ska taxi.");
+
+
+            //list
+            string search = "SELECT * FROM t_person LIMIT 5";
+
+            var cmd2 = new SQLiteCommand(search, con);
+
+            SQLiteDataReader rdr = cmd2.ExecuteReader();
+
+
+
+
+            while (rdr.Read())
+            {
+
+                Console.WriteLine($" {rdr.GetInt32(0), -7} {rdr.GetString(1)}");
+            }
+            rdr.Close();
+
+            
+        }
+        private void command(string command, SQLiteCommand sqlcmd)
+        {
+            sqlcmd.CommandText = command;
+            sqlcmd.ExecuteNonQuery();
+        }
+
+
+
+        private List<string> getInfo(int id)
+        {
+            var con = new SQLiteConnection(cs);
+            con.Open();
+
+            
+
+
+            string search = "SELECT name FROM t_person WHERE id = " + id.ToString();
+
+            List<string> results = new List<string>();
+            var cmd = new SQLiteCommand(search, con);
+
+            Console.WriteLine("Getting name. SQL : "+ search);
+            SQLiteDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                results.Add(rdr.GetString(0));
+
+            } rdr.Close();
+
+            search = "SELECT gatuadress, postadress FROM t_adress WHERE id = " + id.ToString();
+            cmd = new SQLiteCommand(search, con);
+            rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                results.Add(rdr.GetString(0));
+                results.Add(rdr.GetInt32(1).ToString());
+
+            } rdr.Close();
+            search = "SELECT distributionstyp FROM t_distribution WHERE id = " + id.ToString();
+            cmd = new SQLiteCommand(search, con);
+            rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                results.Add(rdr.GetString(0));
+
+            }
+            rdr.Close();
+
+
+            //string result = "";
+
+            //foreach(var res in results)
+            //{
+            //    result = resul
+            //}
+
+            return results;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // search
+            var con = new SQLiteConnection(cs);
+            con.Open();
+
+            var cmd = new SQLiteCommand(stm, con);
+
+            bert.Controls.Clear();
+            bert.Controls.Add(new Label() { Text = "Namn" }, 0, 0);
+            bert.Controls.Add(new Label() { Text = "Gatuadress" }, 1, 0);
+            bert.Controls.Add(new Label() { Text = "Postadress" }, 2, 0);
+            bert.Controls.Add(new Label() { Text = "Distributionstyp" }, 3, 0);
+
+
+            string search = "SELECT id FROM t_person WHERE name LIKE '%" + textBox1.Text + "%'";
+
+            var searchway = comboBox1.Text;
+
+            if (searchway=="Namn")
+            {
+                search = "SELECT id FROM t_person WHERE name LIKE '%" + textBox1.Text + "%'";
+            } else if(searchway=="Gatuadress")
+            {
+                search = "SELECT id FROM t_adress WHERE gatuadress LIKE '%" + textBox1.Text + "%'";
+            }
+            else if (searchway == "Postadress")
+            {
+                search = "SELECT id FROM t_adress WHERE postadress LIKE '%" + textBox1.Text + "%'";
+            }
+            else if (searchway == "Distributionstyp")
+            {
+                search = "SELECT id FROM t_distribution WHERE distributionstyp LIKE '%" + textBox1.Text + "%'";
+            }
+            cmd = new SQLiteCommand(search, con);
+            SQLiteDataReader idrdr = cmd.ExecuteReader();
+                
+
+            while (idrdr.Read())
+            {
+                int id = idrdr.GetInt32(0);
+
+                string info = "";
+
+                foreach(var inf in getInfo(id))
+                {
+                    info = info + inf.ToString();
+
+                    Console.WriteLine(inf);
+                    bert.Controls.Add(new Label() { Text = inf });
+                        
+                }
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+
+            var con = new SQLiteConnection(cs);
+            con.Open();
+
+            var cmd = new SQLiteCommand(stm, con);
+
+
+            string name = namn.Text;
+            string gata = gatuadress.Text;
+            string post = postadress.Text;
+            string dist = distributionstyp.Text;
+
+
+            if(name==String.Empty || gata==String.Empty || post==String.Empty)
+            {
+                System.Windows.Forms.MessageBox.Show("Du bör fylla i alla rutor!");
+                return;
+            }
+
+            command("INSERT INTO t_person(name) VALUES('"+name+"')", cmd);
+            command("INSERT INTO t_adress(gatuadress, postadress) VALUES('"+gata+"', '"+post+"')", cmd);
+            command("INSERT INTO t_distribution(distributionstyp) VALUES('"+dist+"')", cmd);
+
+            System.Windows.Forms.MessageBox.Show(namn+" registerad!");
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabort_Click(object sender, EventArgs e)
+        {
+            var con = new SQLiteConnection(cs);
+            con.Open();
+
+            var cmd = new SQLiteCommand(stm, con);
+
+
+            string id = textBox2.Text;
+            command("DELETE FROM t_person WHERE id = " + id.ToString(), cmd);
+            command("DELETE FROM t_adress WHERE id = " + id.ToString(), cmd);
+            command("DELETE FROM t_distribution WHERE id = " + id.ToString(), cmd);
+
+            System.Windows.Forms.MessageBox.Show("Borttagningen lyckades!");
+        }
+    }
+}
